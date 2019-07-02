fcc359e9deb4f6f48eadc36d3844d40402bef7e8
cameaann <cameaann@gmail.com>
2019-07-02 23:15:21 +0300

    form and bgvideo added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StoryComponent } from './story/story.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StoryComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
