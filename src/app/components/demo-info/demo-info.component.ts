984affbc1f9b55b7302a067c1f1fc53231b55c95
Anna <cameaann@gmail.com>
2022-02-03 17:54:47 +0300

    added block-demo, edited nav

--- src/app/components/demo-info/demo-info.component.ts
+++ src/app/components/demo-info/demo-info.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-info.component.scss']
 })
 export class DemoInfoComponent implements OnInit {
-
+  userLanguage;
+  brString;
   constructor() { }
 
   ngOnInit() {
+    this.userLanguage = window.navigator.language;
+    console.log(this.userLanguage);
+
+    if(this.userLanguage == "ru"){
+      this.brString = "Это демо-проект для портфолио Шестаковой Анны. Мой email: cameaann@gmail.com";
+
+    }
+    else{
+      this.brString = "This demo-project is made for Anna Shestakova's portfolio. My email: cameaann@gmail.com";
+    }
   }
 
 }
